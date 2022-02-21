9b9fff98b52cf44495b177bbe66a474b1e977371
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-02-21 13:36:00 +0800

    secrets done

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,20 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    # Please implement the secret_add/1 function
-  end
+  require Bitwise
 
-  def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
-  end
+  def secret_add(secret), do: &(&1 + secret)
 
-  def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
-  end
+  def secret_multiply(secret), do: &Kernel.*(&1, secret)
 
-  def secret_and(secret) do
-    # Please implement the secret_and/1 function
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
-  end
+  def secret_and(secret), do: &Bitwise.&&&(&1, secret)
+
+  def secret_xor(secret), do: &Bitwise.^^^(&1, secret)
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    &(secret_function1.(&1)
+      |> secret_function2.())
   end
 end
